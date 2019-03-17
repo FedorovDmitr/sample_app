989490ecd14d4df62cc58101f1a4d99d1bedc0fc
Dmitry Fedorov <twich@bk.ru>
2019-03-18 00:49:34 +0300

    13.2.3

---
+++ app/controllers/mictoposts_controller.rb
@@ -0,0 +1,2 @@
+class MictopostsController < ApplicationController
+end
---
+++ test/controllers/mictoposts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MictopostsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
